369d783fb04f8b89bf38dbcbd8606d1f0e0e4029
PirminVdB <pirmin.vdb@gmail.com>
2015-03-04 07:48:06 +0100

    Dom led should be only one led and not an RGBLed

--- DOM/Led.py
+++ DOM/Led.py
@@ -1,34 +1,26 @@
 __author__ = 'Pirmin'
 
-class RGBLed(object):
+MAX_BRIGHTNESS = 256
+
+class Led(object):
 
     def __init__(self):
-        self.red = 0
-        self.green = 0
-        self.blue = 0
+        self.brightness = 0
 
     def set_color(self, red, green, blue):
         self.set_red(red)
         self.set_green(green)
         self.set_blue(blue)
 
-    def set_red(self, value):
-        assert 0 <= value , "state of red %s is invalid, state must be an integer larger than or equal to 0" % value
-        self.red = value
+    def set_brightness(self, brightness):
+        assert 0 <= brightness <= MAX_BRIGHTNESS  , "brightness of led %s is invalid, brightness must be between 0 and 256 with borders" % brightness
+        self.brightness = brightness
 
-    def set_green(self, value):
-        assert 0 <= value , "state of green %s is invalid, state must be an integer larger than or equal to 0" % value
-        self.green = value
-
-    def set_blue(self, value):
-        assert 0 <= value , "state of blue %s is invalid, state must be an integer larger than or equal to 0" % value
-        self.blue = value
-
-    def get_red(self):
+    def get_brightness(self):
         return self.red
 
-    def get_green(self):
-        return self.green
+    def turn_off(self):
+        self.set_brightness(0)
 
-    def get_blue(self):
-        return self.blue
+    def turn_on(self):
+        self.set_brightness(MAX_BRIGHTNESS)
